1a0c3014ea35eb9e3540fe556934ec4a1ec1ec11
peet1126 <pbmcdonald7@gmail.com>
2021-09-04 12:51:18 -0500

    Added Video ECOMM Experiments and Edited the Navbar transition on scroll

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
   // this will work when your window scrolled.
   var scroll = $(window).scrollTop(); //getting the scrolling height of window
-  if (scroll > 100) {
+  if (scroll > 1) {
     $('.header').addClass('header-scrolled');
   } else {
     $('.header').removeClass('header-scrolled');
